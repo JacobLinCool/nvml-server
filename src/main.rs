15ee88b8732369be3119e006ec130a161aba8679
JacobLinCool <jacoblincool@gmail.com>
2024-06-09 23:58:27 +0800

    feat: cors headers

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod error;
 mod routes;
 
 use axum::{routing::get, Router};
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
 
 #[tokio::main]
 async fn main() {
@@ -9,7 +13,9 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(routes::root))
-        .route("/stats", get(routes::stats::stats));
+        .route("/stats", get(routes::stats::stats))
+        .layer(CorsLayer::new().allow_origin(Any))
+        .layer(TraceLayer::new_for_http());
 
     let port = std::env::var("PORT").unwrap_or_else(|_| "21005".to_string());
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
